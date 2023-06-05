9e053d528dacd4390c78ccce2a02f09f8f9c0904
Jun <junyuplee@mines.edu>
2023-06-04 20:49:41 -0600

    randomly generated position method

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-import pygame as pg, sys
+import pygame as pg, sys, random, math
+
+vec2, vec3 = pg.math.Vector2, pg.math.Vector3
 
 RES = WIDTH, HEIGHT = 1000, 800
 NUM_STARS = 100
+CENTER = math.vec2(WIDTH // 2, HEIGHT // 2)
+COLORS = 'red green blue orange purple cyan'.split()
+Z_DISTANCE = 40 # distance from which stars begin to move
 
 class Star:
     def __init__(self, app):
-        pass
+        self.screen = app.screen
+        self.pos3d = None
+
+    def get_pos3d(self):
+        angle = random.uniform(0, 2*math.pi)
+        radius = random.randrange(HEIGHT)
+        x = radius * math.sin(angle)
+        y = radius * math.cos(angle)
+        return vec3(x, y, Z_DISTANCE)
 
     def update(self):
         pass
@@ -26,10 +39,12 @@ class App:
         self.screen = pg.display.set_mode(RES)
         pg.display.set_caption('Wormhole')
         self.clock = pg.time.Clock()
+        self.starfield = Starfield(self)
     
     def run(self):
         while True:
             self.screen.fill('black')
+            self.starfield.run()
 
             pg.display.flip()
             [sys.exit() for e in pg.event.get() if e.type == pg.QUIT]
